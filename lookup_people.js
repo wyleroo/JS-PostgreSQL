8980c19230cf9db8e0bf1fba009bc3784d5dc414
Wyatt Robinson <thewyatt@gmail.com>
2017-03-28 21:41:49 +0000

    Returns query with proper data but only one insert so far. Need to stack inserts.

--- lookup_people.js
+++ lookup_people.js
@@ -1,6 +1,6 @@
 const args = process.argv[2];
-
 var pg = require('pg');
+const settings = require("./settings");
 
 var client = new pg.Client({
   user     : settings.user,
@@ -14,12 +14,11 @@ var client = new pg.Client({
 client.connect(function (err) {
   if (err) throw err;
 
-  client.query('DROP TABLE IF EXISTS famous_people CASCADE');
-
-  const createTable = client.query('CREATE TABLE famous_people (id BIGSERIAL PRIMARY KEY, first_name VARCHAR(50), last_name VARCHAR(50), birthdate DATE');
+  const createTable = client.query('DROP TABLE IF EXISTS famous_people CASCADE; CREATE TABLE famous_people(id BIGSERIAL PRIMARY KEY, first_name VARCHAR(50), last_name VARCHAR(50), birthdate DATE)');
 
   createTable.on('end', () => {
-    const insertTable = client.query('INSERT INTO famous_people (first_name, last_name, birthdate) VALUES ('Paul', 'Rudd', '1969-04-06')');
+    const insertTable = client.query('INSERT INTO famous_people (first_name, last_name, birthdate) VALUES ($1, $2, $3)', ['Paul', 'Rudd', '1969-04-06']);
+
     insertTable.on('end', () => {
       client.query('SELECT * FROM famous_people WHERE first_name=$1', [args], function (err, result) {
       if (err) throw err;
@@ -32,8 +31,10 @@ client.connect(function (err) {
   });
 });
 
-on('end', () => { client.end(); });
+// on('end', () => { client.end(); });
 
+
+// 'INSERT INTO famous_people (first_name, last_name, birthdate) VALUES (''Paul'', ''Rudd'', '''')')
 
   // client.query('INSERT INTO famous_people (first_name, last_name, birthdate) VALUES ('Abraham', 'Lincoln', '1809-02-12')');
 
